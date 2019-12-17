0435c071ebffc5b7f1cb557d5ba553f2e570ec3b
Tanis Pan <e.pan.gar@gmail.com>
2019-12-17 01:15:37 +0100

    Fetched

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const tasks = ["Take out the trash", "Shovel the driveway", "Walk the dog"];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = React.createElement('ol', null, 
+  tasks.map((task, index) => <li key="index"> {task} </li>)
+)
+
+ReactDOM.render(element, document.getElementById('root'));
+
