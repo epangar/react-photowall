c02411d589d7c838b86aeb15763784ec713f322a
Tanis Pan <e.pan.gar@gmail.com>
2019-12-17 15:23:03 +0100

    JS y JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//React
+
 const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
-const element = React.createElement('ol', null,
-  tasks.map((task, index)=> React.createElement('li', {key: index}, task))
-);
+const taskList = React.createElement('ol', null,
+tasks.map((task, index)=> React.createElement('li', {key: index}, task))
+)
+const element = React.createElement('div', null, taskList)
 
-ReactDOM.render(element, document.getElementById('root'));
+
+
+//ReactDOM.render(element, document.getElementById('root'));
+
+//JSX
+
+const names = ["Alexander", "Bernard", "Charles"]
+
+const JSXelement = <div>
+                      <h1> JSX Name list</h1>
+                      <ul> 
+
+                      {names.map((name, position) => <li key = {position}> {name} </li>)}
+                      
+                      </ul>
+                    </div>
+
+const myArray= [element, JSXelement]
+ReactDOM.render(myArray, document.getElementById('root'));
