2acde31bec2c34fd30c457d16fdf3e9c2a037bfd
Tanis Pan <e.pan.gar@gmail.com>
2019-12-17 01:52:08 +0100

    SOme fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const tasks = ["Take out the trash", "Shovel the driveway", "Walk the dog"];
+const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
-const element = React.createElement('ol', null, 
-  tasks.map((task, index) => <li key="index"> {task} </li>)
-)
+const element = React.createElement('ol', null,
+  tasks.map((task, index)=> React.createElement('li', {key: index}, task))
+);
 
-ReactDOM.render(element, document.getElementById('root'));
-
+ReactDOM.render(element, document.getElementById('root'))
